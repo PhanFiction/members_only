7afca269ab0d0abfeb696985110fbb17f243f49e
Steve <stevewu24@gmail.com>
2024-06-21 13:52:59 -0700

    created and added config created routes config created devise config created devise.en.yml config

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # devise_for :users path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register', sign_up: 'cmon_let_me_in' }
+  devise_for :users
+  resources :users do
+    resources :posts
+  end
 
+  resources :posts, only: [:new, :create, :index]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  
+  # root to: 'home#index'
+  root "posts#index"
 end
